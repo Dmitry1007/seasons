309ee98da03056416b367c331bbc309cf840f999
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-04 20:33:22 -1000

    Get users geolocation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Yo Yo Yo!</div>;
+  window.navigator.geolocation.getCurrentPosition(
+    // success callback
+    position => console.log(position),
+    // failure callback
+    err => console.log(err)
+  );
+
+  return (
+    <div>Yo Yo Yo!</div>
+  )
 };
 
 ReactDOM.render(
