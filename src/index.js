43b2fe342b08815bca8505a249ab839373822110
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-04 21:36:04 -1000

    Turn App function into a Class based component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    // success callback
-    position => console.log(position),
-    // failure callback
-    err => console.log(err)
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      // success callback
+      position => console.log(position),
+      // failure callback
+      err => console.log(err)
+    );
 
-  return (
-    <div>Yo Yo Yo!</div>
-  )
+    return (
+      <div>Latitude: </div>
+    )
+  }
 };
 
 ReactDOM.render(
