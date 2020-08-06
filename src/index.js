a60cb8c30736e2236eae4ce3dcb078e689707231
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-05 20:01:48 -1000

    Initialize state in app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    // super is a reference to the parents(React.Component)
+    // constructor function
+    super(props);
+    this.state = { lat: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       // success callback
-      position => console.log(position),
+      position => console.log(position.coords.latitude),
       // failure callback
       err => console.log(err)
     );
