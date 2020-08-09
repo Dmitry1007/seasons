a47a0ca92d6373c5d04ba6bfd8de6c42af82ac70
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-09 09:13:07 -1000

    Add some bomb style

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
@@ -22,10 +23,10 @@ const SeasonDisplay = (props) => {
   const { text, iconName } = seasonConfig[season]
 
   return (
-    <div>
-      <i className={`${iconName} icon big`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left ${iconName} icon massive`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon big`} />
+      <i className={`icon-right ${iconName} icon massive`} />
     </div>
   )
 };
